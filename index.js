3c44cf54d906315a43ffe7859f3e37f49877b381
Taran <taranpal.singh23@gmail.com>
2019-07-24 13:35:58 +0530

    tap to create a new ball

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var time = 1000;
 var W = window.innerWidth;
 var H = window.innerHeight;
 
-const N = 10;
+const N = 1;
 var gravity = 0.3;
 // var traction = 0.005;
 var bounce_factor = 1;
@@ -27,31 +27,48 @@ canvas.height = H;
 
 var ball = []
 
+function newBall(mousex, mousey){
+  ball.push(new Ball(mousex, mousey));
+  console.log(ball.length);
+}
+
+// newBall();
+// newBall();
 
 for(let j=0; j<N; j++){
-  ball.push(new Ball());
-}// var ball2 = new Ball();
+  newBall();
+}
+
+// setInterval(function(){
+//   newBall();
+// }, 3000);
+
+// var ball2 = new Ball();
 
 var mousex = 0;
 var mousey = 0;
 
-addEventListener("mousemove", function(){
+addEventListener("click", function(){
+  console.log("inside");
   mousex = event.clientX;
   mousey = event.clientY;
+  newBall(mousex, mousey);
 })
 
-function createObstacle(){
-  var obstacle = new Obstacle();
-  return obstacle;
-}
+// function createObstacle(){
+//   var obstacle = new Obstacle();
+//   return obstacle;
+// }
 
 $(document).ready( function renderFrame() {
 
   requestAnimationFrame(renderFrame);
   ctx.clearRect(0, 0, W, H);
 
-  for(let i=0; i<N; i++){
+  for(let i=0; i<ball.length; i++){
 
+    console.log(ball.length);
+    // console.log(ball[i].vy);
     ball[i].vy += gravity;
     // vx = (vx < 0 ? vx+traction : vx-traction);
     ball[i].x += ball[i].vx;
@@ -73,16 +90,16 @@ $(document).ready( function renderFrame() {
       ball[i].vy *= -bounce_factor;
     }
 
-    if((mousex >= (ball[i].x - ball[i].radius)) &&
-      (mousex <= (ball[i].x + ball[i].radius)) &&
-      (mousey >= (ball[i].y - ball[i].radius)) &&
-      (mousey <= (ball[i].y + ball[i].radius)))
-    {
-      ball[i].newRadius = ball[i].radius + 40;
-    }
-    else{
-      ball[i].newRadius = ball[i].radius;
-    }
+    // if((mousex >= (ball[i].x - ball[i].radius)) &&
+    //   (mousex <= (ball[i].x + ball[i].radius)) &&
+    //   (mousey >= (ball[i].y - ball[i].radius)) &&
+    //   (mousey <= (ball[i].y + ball[i].radius)))
+    // {
+    //   ball[i].newRadius = ball[i].radius + 40;
+    // }
+    // else{
+    //   ball[i].newRadius = ball[i].radius;
+    // }
     ball[i].draw(ctx);
   }
 }());
@@ -96,17 +113,14 @@ function getRandomColor() {
   return color;
 }
 
-function Ball() {
+function Ball(mousex, mousey) {
   this.color = getRandomColor();
   this.radius = Math.random()*50 + 15;
 
-  // this.x = Math.random()*canvas.width + this.radius;
-  // this.y = Math.random()*(canvas.height/4) + this.radius;
+  this.x = (mousex == undefined ? Math.random()*canvas.width : mousex);
+  this.y = (mousey == undefined ? Math.random()*(canvas.height) + this.radius : mousey);
 
-  this.x = Math.random()*canvas.width;
-  this.y = Math.random()*(canvas.height) + this.radius;
-
-  console.log("(x,y) : ", this.x, this.y);
+  // console.log("(x,y) : ", this.x, this.y);
 
   this.newRadius = this.radius;
   var randomVal = Math.random();
@@ -114,7 +128,6 @@ function Ball() {
   this.vx = (randomVal * 10);  // To ensure a value between 6 and 15.
   if( randomVal % 2 > 0.5)  // remainder is in the range [0,1]
     this.vx *= -1;
-  console.log(this.vx);
   this.vy = 0;
 
   this.draw = function(ctx) {
